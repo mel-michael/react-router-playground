e49c02387b67084d079b3a238c0f2377693cb14b
Mel Michael <mel.michael@andela.com>
2018-06-28 14:29:48 +0100

    implement nested route and practice the use of match

--- src/App.js
+++ src/App.js
@@ -48,10 +48,31 @@ const About = () => (
   </div>
 );
 
-const Topics = () => (
+const Topics = ({ match }) => (
   <div>
-    <h2>I am all about the topics</h2>
+    <h2>Topics</h2>
+    <ul>
+      <li>
+        <Link to={`${match.url}/rendering`}>Rendering with React</Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/components`}>Components</Link>
+      </li>
+      <li>
+        <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
+      </li>
+    </ul>
+
+    <Route exact path={match.url} render={() => <h3>Please select a topic.</h3>} />
+    <Route path={`${match.url}/:topicId`} component={Topic} />
   </div>
 );
+
+const Topic = ({ match }) => (
+  <div>
+    <h3>{match.params.topicId}</h3>
+  </div>
+);
+
 
 export default App;
